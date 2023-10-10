b51c559a9f2b4e3f4c9d6d1192040c1ecd60fe21
Kulpot <kulpotwahu@gmail.com>
2023-10-11 06:07:58 +0800

    Add project files.

---
+++ Assembly Division/MyAssembly.asm
@@ -0,0 +1,31 @@
+
+.586
+.model flat, c
+.stack 100h
+.data
+
+.code
+
+doit proc
+
+	; this is a comment i can type whatever i like after a semicolon and assembler will ignore it
+	; 8 * 4 
+
+	;mov eax, 8	
+	;mov ebx, 4
+	;mov edx, 21323	; edx will fill the overflow	
+	; mul eax, ebx	; syntacs error: mul only needs 1 argument	
+	;mul ebx
+	;mul 4
+	
+	mov eax, 0FFFFFFFFh
+	mov ebx, 0FFFFFFFFh
+	mul ebx
+
+	; the went bought. some I and, food to store	; wrong syntacs, semantics, parsing
+	; I went to the store, and I bought some food. ;correct semantics, parse
+	; I went to the run, and I bought some dive.	; wrong semantics
+
+doit endp
+
+end
